97fae086c6d5e2cf0ead4fd6c3016d8881e85ed1
Shiva <sivaram.jebamani@citi.com>
2015-06-03 11:14:09 +0530

    first commit

---
+++ MultipleFieldPoC/MultipleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MultipleTableViewCell.swift
+//  MultipleFieldPoC
+//
+//  Created by Shiva on 18/02/15.
+//  Copyright (c) 2015 Shiva. All rights reserved.
+//
+
+import UIKit
+
+class MultipleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# multipleField-Demo
