b75d8934e012d5abb60af0713b7a0ad0e593f178
Shiva <sivaram.jebamani@citi.com>
2015-06-03 11:38:21 +0530

    1) Implemented multiple field logic on the table cell

--- MultipleFieldPoC/MultipleTableViewCell.swift
+++ MultipleFieldPoC/MultipleTableViewCell.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class MultipleTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
+    
+    func setValueForDescription(values:Dictionary<String, NSAttributedString>?) {
+        if let values = values {
+            var text = NSMutableAttributedString()
+            for (key,value) in values {
+                text.appendAttributedString(value)
+                text.appendAttributedString(NSAttributedString(string: "\n"))
+            }
+            self.lblDescription.attributedText = text
+        } else {
+            self.lblDescription.text = "Hello Hello Hello Hello Hello Hello Hello Hello Hello \n Hello Hello Hello Hello Hello Hello Hello Hello Hello \n Hello Hello Hello Hello Hello Hello Hello Hello Hello"
+            
+        }
+    }
+    
 
 }
--- MultipleFieldPoC/ViewController.swift
+++ MultipleFieldPoC/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var values = Dictionary<String, NSAttributedString>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Configure table dynamic height
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 160.0
+        
+        // Adding Values to Dictionary
+        values.updateValue(self.formatValues("Sivaram", colorValue: UIColor.blackColor()), forKey: "Name")
+        values.updateValue(self.formatValues("Demo Project", colorValue: UIColor.blueColor()), forKey: "Project")
+        values.updateValue(self.formatValues("Ramanujam IT Park Chennai", colorValue: UIColor.grayColor()), forKey: "Location")
+        //values.updateValue(self.formatValues("Tata Consulatancy Services", colorValue: UIColor.redColor()), forKey: "company")
+        //values.updateValue(self.formatValues("Tata Consulatancy Services", colorValue: UIColor.redColor()), forKey: "company1")
+        //values.updateValue(self.formatValues("Tata Consulatancy Services", colorValue: UIColor.redColor()), forKey: "company2")
+        //values.updateValue(self.formatValues("Tata Consulatancy Services", colorValue: UIColor.redColor()), forKey: "company3")
+        // values.updateValue(self.formatValues("Tata Consulatancy Services", colorValue: UIColor.redColor()), forKey: "company4")
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func formatValues(forStr: String, colorValue:UIColor) -> NSAttributedString {
+        var fomattedStr:NSMutableAttributedString? = NSMutableAttributedString(string: forStr)
+        if let fomattedStr = fomattedStr {
+            fomattedStr.addAttribute(NSForegroundColorAttributeName, value: colorValue, range: NSMakeRange(0, fomattedStr.length))
+        }
+        return fomattedStr!
+    }
+    
 }
 
+extension ViewController{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell? {
+        
+        var cell: MultipleTableViewCell
+        
+        if (indexPath.row % 2) == 0 {
+            cell = tableView.dequeueReusableCellWithIdentifier("MultipleCell", forIndexPath: indexPath) as! MultipleTableViewCell
+            cell.lblTitle?.text = "Demo project"
+            cell.setValueForDescription(values)
+        } else {
+            cell = tableView.dequeueReusableCellWithIdentifier("NewCell", forIndexPath: indexPath) as! MultipleTableViewCell
+            cell.lblTitle?.text = "Sivaram kljsdl;f ja;lsdjfl;asdjfl;asjfl;ajs fljsad;fl jasldfjsda Sivaram kljsdl;f ja;lsdjfl;asdjfl;asjfl;ajs fljsad;fl jasldfjsda"
+            cell.lblDescription.text = "iOS Developer sdklfj a;lfjlk;sadjfl;asdjfl;asjd fds "
+            
+        }
+        return cell
+    }
+    
+}
